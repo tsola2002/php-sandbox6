8cf7f6920ab211ec52eace3f1bbda04b121b9edb
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-06-02 15:13:28 +0100

    built in functions

---
+++ 06-FUNCTIONS/functions.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions</title>
+</head>
+<body>
+    <h1>Working With Functions</h1>
+    <?php
+    // BUILT-IN FUNCTIONS
+    //phpinfo();
+
+    // DATE FUNCTIONS 
+    echo date('l');
+    
+    ?>
+</body>
+</html>
---
+++ 06-FUNCTIONS/functions2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions</title>
+</head>
+<body>
+    <h1>Working With Function</h1>
+    <?php
+    // BUILT-IN FUNCTIONS
+    //phpinfo();
+
+    // DATE FUNCTIONS 
+    // echo date('l') ."<br>";
+    echo date(" l dS \of F Y h :i :s A") . "<br>";
+
+    // TIME FUNCTIONS
+     $timestamp = time();
+     echo date('H:i:s', $timestamp) . "<br>";   
+
+     echo date_default_timezone_get() . "<br>";
+
+     date_default_timezone_set('Africa/Lagos');
+
+     echo date('H:i:s', $timestamp) . "<br>";
+
+
+     // CALENDAR FUNCTIONS 
+    $week = cal_to_jd(CAL_GREGORIAN, date("d"), date("m"), date("Y"));
+    
+    echo $week . "<br>";
+
+    $info = cal_info(0);
+    print_r($info);
+    ?>
+</body>
+</html>
