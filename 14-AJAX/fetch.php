7fd0ba87a5e1bc44053866d76cffed474422be44
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-08-13 15:30:34 +0100

    fetch file implemented

--- 14-AJAX/fetch.php
+++ 14-AJAX/fetch.php
@@ -1,43 +1,41 @@
-<?php 
+<?php
 
 require "database.php";
 
-$query = "SELECT * FROM tbl_student";
-$result = mysqli_query($connection, $query);
+$query =  "SELECT * FROM tbl_student";
+$result  = mysqli_query($connection, $query);
 
-echo "<table class='table table-bordered table-stripped'>";
+echo "<table class='table table-bordered table-striped'>";
 echo "<thead class='table-dark'>
         <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
-            <th>Gender</th>
             <th width='160'>Actions</th>
         </tr>
       </thead>
       <tbody>";
 
-        while($row = mysqli_fetch_assoc($result)){
-            echo "<tr>";
-            echo "<td>" . $row['id'] . "</td>";
-            echo "<td>" . $row['name'] . "</td>";
-            echo "<td>" . $row['email'] . "</td>";
-            echo "<td>
-                    <button class='btn btn-sm btn-primary' onclick=\"editStudent(
-                    '" . $row['id'] . "',
-                    '" . $row['name'] . "',
-                    '" . $row['email'] . "',
-                    )\">
-                    Edit</button>
+while ($row = mysqli_fetch_assoc($result)){
+    echo "<tr>
+            <td>{$row['id']}</td>
+            <td>{$row['name']}</td>
+            <td>{$row['email']}</td>
+            <td>
+                <button class='btn btn-sm btn-warning me-1'
+                 onclick=\"editStudent(
+                 '{$row['id']}',
+                 '{$row['name']}',
+                 '{$row['email']}',
+                 )\">
+                 Edit
+                </button>
 
-                    <button class='btn btn-sm btn-danger
-                    ' onclick=\"deleteStudent('" . $row['id'] . "')\">
-                    Delete</button>
-                </td>";
-            echo "</tr>";
-        }
-
-        echo "</tbody>";
-        echo "</table>";
-
-?>
+                <button class='btn btn-sm btn-danger'
+                    onclick=\"deleteStudent({$row['id']})\">
+                    Delete
+                </button>
+            </td>
+           </tr>";
+}
+echo "</tbody></table>";
